1b4d5249df0596762398cf943c48b1b0895567d1
Danntime <danntime@gmail.com>
2022-05-23 00:03:08 +0200

    hotfix

--- Navetas/Assets/scripts/OpacityMidSC.cs
+++ Navetas/Assets/scripts/OpacityMidSC.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MagnetAreaSC : MonoBehaviour
+public class OpacityMidSC : MonoBehaviour
 {
     private void Start()
     {
